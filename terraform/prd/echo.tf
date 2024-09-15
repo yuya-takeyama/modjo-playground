cbf4a17d855b3f7aa9f0e7850e54c43a5e1fad70
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2024-09-16 06:11:46 +0900

    Setup terraform (#6)

---
+++ terraform/prd/echo.tf
@@ -0,0 +1,25 @@
+resource "aws_apprunner_service" "echo" {
+  service_name = "echo"
+
+  source_configuration {
+    image_repository {
+      image_configuration {
+        port = "8080"
+      }
+
+      image_identifier      = "920373013500.dkr.ecr.ap-northeast-1.amazonaws.com/mobb/echo:0.0.20240916053025"
+      image_repository_type = "ECR"
+    }
+
+    authentication_configuration {
+      access_role_arn = "arn:aws:iam::920373013500:role/service-role/AppRunnerECRAccessRole"
+    }
+
+    auto_deployments_enabled = false
+  }
+
+  instance_configuration {
+    cpu    = "256"
+    memory = "512"
+  }
+}
---
+++ terraform/stg/echo.tf
@@ -0,0 +1,25 @@
+resource "aws_apprunner_service" "echo" {
+  service_name = "echo"
+
+  source_configuration {
+    image_repository {
+      image_configuration {
+        port = "8080"
+      }
+
+      image_identifier      = "615299752259.dkr.ecr.ap-northeast-1.amazonaws.com/mobb/echo:latest"
+      image_repository_type = "ECR"
+    }
+
+    authentication_configuration {
+      access_role_arn = "arn:aws:iam::615299752259:role/service-role/AppRunnerECRAccessRole"
+    }
+
+    auto_deployments_enabled = false
+  }
+
+  instance_configuration {
+    cpu    = "256"
+    memory = "512"
+  }
+}
