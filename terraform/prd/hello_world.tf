9304ed1072763f3c6dcb18170af2ce73c7e3ee9f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2024-09-15 21:10:36 +0000

    Setup terraform

---
+++ terraform/prd/hello_world.tf
@@ -0,0 +1,25 @@
+resource "aws_apprunner_service" "hello_world" {
+  service_name = "hello-world"
+
+  source_configuration {
+    image_repository {
+      image_configuration {
+        port = "8080"
+      }
+
+      image_identifier      = "920373013500.dkr.ecr.ap-northeast-1.amazonaws.com/mobb/hello-world:0.0.20240916052231"
+      image_repository_type = "ECR"
+    }
+
+    authentication_configuration {
+      access_role_arn = "arn:aws:iam::920373013500:role/service-role/AppRunnerECRAccessRole"
+    }
+
+    auto_deployments_enabled = false
+  }
+
+  instance_configuration {
+    cpu    = "256"
+    memory = "512"
+  }
+}
